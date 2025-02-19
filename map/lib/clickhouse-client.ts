1b034584f474a54ac73871d4462941d7043f0b7f
Nikhil Rajaram <nikhil.rajaram@teamengine.io>
2025-02-19 14:48:21 -0500

    adjust get client util

--- map/lib/clickhouse-client.ts
+++ map/lib/clickhouse-client.ts
@@ -1,14 +1,16 @@
 import { createClient } from "@clickhouse/client";
 
-let client;
+const querySettings = {
+  // todo: tune for production
+  // https://altinity.com/blog/unraveling-the-mystery-of-idle-threads-in-clickhouse
+  // max_threads: 1,
+  // max_memory_usage: "8G",
+} as const;
 
-export const getClient = () => {
-  return (
-    client ||
-    createClient({
-      url: process.env.CLICKHOUSE_URL,
-      username: process.env.CLICKHOUSE_USER,
-      password: process.env.CLICKHOUSE_PASSWORD,
-    })
-  );
-};
+export const getClient = () =>
+  createClient({
+    url: process.env.CLICKHOUSE_URL,
+    username: process.env.CLICKHOUSE_USER,
+    password: process.env.CLICKHOUSE_PASSWORD,
+    clickhouse_settings: querySettings,
+  });
